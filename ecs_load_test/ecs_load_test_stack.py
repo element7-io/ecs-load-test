7156cbb16ff666dec9e30b9470e7bc990165e820
Gert Leenders <leenders.gert@gmail.com>
2020-12-29 13:47:40 +0100

    Added ecs cluster and ecs fargate service

--- ecs_load_test/ecs_load_test_stack.py
+++ ecs_load_test/ecs_load_test_stack.py
@@ -1,9 +1,23 @@
-from aws_cdk import core
-
+from aws_cdk import (
+    core, aws_ec2 as ec2,
+    aws_ecs as ecs,
+    aws_ecs_patterns as ecs_patterns
+)
 
 class EcsLoadTestStack(core.Stack):
 
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        vpc = ec2.Vpc(self, "ecs-load-test", cidr="10.0.0.0/22", max_azs=3)
+
+        cluster = ecs.Cluster(self, "load-test-cluser", vpc=vpc)
+
+        ecs_patterns.ApplicationLoadBalancedFargateService(self, "test-service",
+            cluster=cluster,            # Required
+            cpu=512,                    # Default is 256
+            desired_count=6,            # Default is 1
+            task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+                image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample")),
+            memory_limit_mib=2048,      # Default is 512
+            public_load_balancer=True)  # Default is False
